68a6da9897afe8a689e05ee48a138d84b1396781
dannycardenas-project <mygithubemail2024@gmail.com>
2024-07-18 20:29:57 -0500

    Completed code for guessinggame.sh bash script

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,33 @@
 #!/bin/bash
+
+# This program asks the user to guess the number of files in the current directory
+
+pwd=`pwd`
+
+echo
+
+function num_files {
+        ls -la | grep -v "total" | wc -l
+}
+
+number=$(num_files)
+
+echo "This is a game to guess how many files are in the current directory"
+echo -n "Enter a guess: "
+read guess
+
+while [[ $guess != $number ]] 
+do
+        if [[ $guess -lt $number ]]
+        then
+                echo "Too low "
+        else 
+                echo "Too high"
+        fi
+        echo -n "Enter a new guess: "
+        read guess
+done
+echo
+echo -e "\tCongratulations! You have guessed the correct number!"
+echo
+
